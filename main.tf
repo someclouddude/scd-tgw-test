7ed2b0c99dd9a32e9a0db43e88322fc2a4286d59
Ian Evans <someclouddude@gmail.com>
2019-05-04 07:39:29 -0500

    setting asn

--- main.tf
+++ main.tf
@@ -1,21 +1,22 @@
 provider "vault" {
-    address = "https://vault.someclouddude.com:8200"
+  address = "https://vault.someclouddude.com:8200"
 }
 
-data "vault_generic_secret" "aws_keys_scd"{
-    path = "aws/kv/someclouddude"
+data "vault_generic_secret" "aws_keys_scd" {
+  path = "aws/kv/someclouddude"
 }
 
 provider "aws" {
-    access_key = "${data.vault_generic_secret.aws_keys_scd.data["access_key_id"]}"
-    secret_key = "${data.vault_generic_secret.aws_keys_scd.data["secret_access_key"]}"
-    region = "us-west-2"
+  access_key = "${data.vault_generic_secret.aws_keys_scd.data["access_key_id"]}"
+  secret_key = "${data.vault_generic_secret.aws_keys_scd.data["secret_access_key"]}"
+  region     = "us-west-2"
 }
 
 module "tgw-test" {
-    source = "git::git@github.com:someclouddude/scd-tgw//tgw"
+  source = "git::git@github.com:someclouddude/scd-tgw//tgw"
 
-    description = "Creating a test TGW using all default values from the module."
+  amazon_side_asn = 64519
+  description     = "Creating a test TGW using all default values from the module."
 }
 
 module "tgw_rt" {
