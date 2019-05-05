8459536ab4d2a50b7e3121e6bfbb5ed0549d0536
Ian Evans <someclouddude@gmail.com>
2019-05-05 18:31:20 -0500

    adding scd_prd

--- tcds-tmna-route-table.tf
+++ tcds-tmna-route-table.tf
@@ -31,3 +31,23 @@ module "to_scd_dev" {
   tgw_attach_id   = "tgw-attach-00ebca0e814511f9e"
   tgw_rt_id       = "${module.tcds_dmp-tmna_mq.tgw_rt_id}"
 }
+
+# Route, route table association and propagation for scd_prd
+module "route_table_association_for_scd_prd" {
+  source        = "git::git@github.com:someclouddude/scd-tgw//tgw-rt-assoc"
+  tgw_attach_id = "tgw-attach-005275a474f461d36"
+  tgw_rt_id     = "${module.tcds_dmp-tmna_mq.tgw_rt_id}"
+}
+
+module "route_table_propagation_for_scd_prd" {
+  source        = "git::git@github.com:someclouddude/scd-tgw//tgw-rt-prop"
+  tgw_attach_id = "tgw-attach-005275a474f461d36"
+  tgw_rt_id     = "${module.tcds_dmp-tmna_mq.tgw_rt_id}"
+}
+
+module "to_scd_dev" {
+  source          = "git::git@github.com:someclouddude/scd-tgw//tgw-rt-route"
+  dest_cidr_block = "192.168.101.0/24"
+  tgw_attach_id   = "tgw-attach-005275a474f461d36"
+  tgw_rt_id       = "${module.tcds_dmp-tmna_mq.tgw_rt_id}"
+}
