a6ca48b90bc91c295b1e0541ba0f8b6f18a69403
Ian Evans <someclouddude@gmail.com>
2019-05-05 17:52:04 -0500

    adding all the things

--- tcds-tmna-route-table.tf
+++ tcds-tmna-route-table.tf
@@ -11,6 +11,20 @@ module "tcds_dmp-tmna_mq" {
   tags = "${merge(var.tags, local.tgw_rt_tags)}"
 }
 
+
+# Route, route table association and propagation for scd_dev
+module "route_table_association_for_scd_dev" {
+  source        = "git::git@github.com:someclouddude/scd-tgw//tgw-rt-assoc"
+  tgw_attach_id = "tgw-attach-00ebca0e814511f9e"
+  tgw_rt_id     = "${module.tcds_dmp-tmna_mq.tgw_rt_id}"
+}
+
+module "route_table_propagation_for_scd_dev" {
+  source        = "git::git@github.com:someclouddude/scd-tgw//tgw-rt-prop"
+  tgw_attach_id = "tgw-attach-00ebca0e814511f9e"
+  tgw_rt_id     = "${module.tcds_dmp-tmna_mq.tgw_rt_id}"
+}
+
 module "to_scd_dev" {
   source          = "git::git@github.com:someclouddude/scd-tgw//tgw-rt-route"
   dest_cidr_block = "192.168.101.0/24"
