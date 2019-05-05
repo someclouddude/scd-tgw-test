57110c32f596e261db20364243623067df7d25c6
Ian Evans <someclouddude@gmail.com>
2019-05-05 17:08:20 -0500

    maybe arn?

--- resource-shares.tf
+++ resource-shares.tf
@@ -8,6 +8,6 @@ module "tgw_tcds_tmna_share" {
   source          = "git::git@github.com:someclouddude/scd-resource-manager"
   share_name      = "tgw_tcds_tmna_share"
   resource_arn    = "${module.tgw-tcds_dmp-tmna_mq.tgw_arn}"
-  share_principal = "ou-ycsu-37m07fn3"
+  share_principal = "arn:aws:organizations::909295938269:ou/o-knu9g6q8sh/ou-ycsu-37m07fn3"
   tags            = "${merge(var.tags, local.ram_tags)}"
 }
