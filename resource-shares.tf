a8cf88ce261ca8e5ffaa0c9bd1ed519a323a671e
Ian Evans <someclouddude@gmail.com>
2019-05-05 15:22:40 -0500

    corrected to ref arn not id

--- resource-shares.tf
+++ resource-shares.tf
@@ -7,7 +7,7 @@ locals {
 module "tgw_tcds_tmna_share" {
   source          = "git::git@github.com:someclouddude/scd-resource-manager"
   share_name      = "tgw_tcds_tmna_share"
-  resource_arn    = "${module.tgw-tcds_dmp-tmna_mq.tgw_id}"
+  resource_arn    = "${module.tgw-tcds_dmp-tmna_mq.tgw_arn}"
   share_principal = "375886667331"
   tags            = "${merge(var.tags, local.ram_tags)}"
 }
