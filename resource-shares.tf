e5aca4b1aee90e3413b59dd357cd39a2988fe724
Ian Evans <someclouddude@gmail.com>
2019-05-05 13:06:38 -0500

    wtf?

--- resource-shares.tf
+++ resource-shares.tf
@@ -6,7 +6,7 @@ locals {
 
 
 module "tgw_tcds_tmna_share" {
-    source = "git::git@github.com:someclouddude/scd-resource-manager/?ref=master"
+    source = "git::git@github.com:someclouddude/scd-resource-manager/"
     share_name = "tgw_tcds_tmna_share"
 
     resource_arn = "${module.tcds_dmp-tmna_mq.tgw_id}"
