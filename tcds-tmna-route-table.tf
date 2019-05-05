674a06920a2976f4d65aac1172628abf7c32499b
Ian Evans <someclouddude@gmail.com>
2019-05-05 17:43:51 -0500

    corrected output used for route table id

--- tcds-tmna-route-table.tf
+++ tcds-tmna-route-table.tf
@@ -15,5 +15,5 @@ module "to_scd_dev" {
   source          = "git::git@github.com:someclouddude/scd-tgw//tgw-rt-route"
   dest_cidr_block = "192.168.101.0/24"
   tgw_attach_id   = "tgw-attach-00ebca0e814511f9e"
-  tgw_rt_id       = "${module.tcds_dmp-tmna_mq.tgw_route_id}"
+  tgw_rt_id       = "${module.tcds_dmp-tmna_mq.tgw_rt_id}"
 }
