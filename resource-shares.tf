46e7ebc1238f5355d32bd12a0d24d803eb523670
Ian Evans <someclouddude@gmail.com>
2019-05-05 13:12:37 -0500

    vars correction

--- resource-shares.tf
+++ resource-shares.tf
@@ -1,17 +1,13 @@
 locals {
   ram_tags = {
-      shared = "yes"
+    shared = "yes"
   }
 }
 
-
 module "tgw_tcds_tmna_share" {
-    source = "git::git@github.com:someclouddude/scd-resource-manager"
-    share_name = "tgw_tcds_tmna_share"
-
-    resource_arn = "${module.tcds_dmp-tmna_mq.tgw_id}"
-    
-    principal = "375886667331"
-
-    tags = "${merge(var.tags, local.ram_tags)}"
+  source          = "git::git@github.com:someclouddude/scd-resource-manager"
+  share_name      = "tgw_tcds_tmna_share"
+  resource_arn    = "${module.tgw-tcds_dmp-tmna_mq.tgw_id}"
+  share_principal = "375886667331"
+  tags            = "${merge(var.tags, local.ram_tags)}"
 }
